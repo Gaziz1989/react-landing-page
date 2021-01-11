bb045a5317cc0098d4e3c093de2484b87ebf59bf
ZGaziz <g.zhaiyk@p-s.kz>
2021-01-11 19:27:37 +0600

    aaaaa

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <video src='videos/video-1.mp4' autoPlay loop muted controls type="video/mp4"/>
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
---
+++ src/pages/SignUp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Signup() {
+	return <h1 className='sign-up'>
+		SIGN UP
+	</h1>
+}
